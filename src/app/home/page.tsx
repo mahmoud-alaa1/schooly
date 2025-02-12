e2aaa12346cb04556ef7f5189ac14b00dde93709
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-02-12 22:35:31 +0200

    feat: add UpcomingLessons component to display upcoming lessons on the home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import SignoutButton from "@/components/SignoutButton";
+import UpcomingLessons from "@/components/UpcomingLessons";
 export default async function Page() {
   return (
     <MaxWidthWrapper className="py-12">
       <SignoutButton />
+      <UpcomingLessons />
     </MaxWidthWrapper>
   );
 }
