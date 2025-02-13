4b8d469df3d5780657baf18f1e765cfdbc60ff2f
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-02-13 22:57:16 +0200

    feat: update Badge component styles, enhance Lesson component to use new badge text logic, and refactor utility functions for upcoming lessons

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,18 +1,18 @@
 import { TLesson } from "@/types/lessons";
 import Image from "next/image";
 import { Badge } from "./ui/badge";
-import { getTimeUntilStart } from "@/lib/utils";
+import { getUpcomingLessonsBadgeText } from "@/lib/utils";
 
 export default function Lesson({ details }: { details: TLesson }) {
-  const { date, from, grade, subject, title } = details;
-  const { text, variant } = getTimeUntilStart(date, from);
+  const { date, from, grade, subject, title, to } = details;
+  const { text, variant } = getUpcomingLessonsBadgeText(date, from, to);
   return (
-    <li className="min-h-20 grid grid-cols-[auto_3fr_auto] gap-x-3 border-b pt-2 pb-4 last-of-type:border-none">
-      <Image height={52} width={52} src="/atom.png" alt={subject} />
+    <li className="mb-3 min-h-20 grid grid-cols-[auto_3fr_auto] gap-x-3 border-b pt-2 pb-4 last-of-type:border-none">
+      <Image height={52} width={52} src={`/physics.png`} alt={subject} />
       <article className="">
         <header>
           <h3 className="text-[#00000073]">
-            {subject}/{grade}
+            {subject} / {grade}
           </h3>
         </header>
         <p>{title}</p>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,7 @@ import {
   differenceInMinutes,
   differenceInHours,
   isTomorrow,
+  isWithinInterval,
 } from "date-fns";
 import { arEG } from "date-fns/locale";
 import { clsx, type ClassValue } from "clsx";
@@ -17,22 +18,49 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 type TBadgeVariant = "blue" | "orange" | "red";
+type TBadgeTimeUpcomingLessons = { text: string; variant: TBadgeVariant };
 
-export function getTimeUntilStart(
-  dateStr: string,
-  startTimeStr: string
-): { text: string; variant: TBadgeVariant } {
-  const secondsDifference = differenceInSeconds(new Date(dateStr), new Date());
-  const miniutesDiffernce = differenceInMinutes(new Date(dateStr), new Date());
-  const hoursDifference = differenceInHours(new Date(dateStr), new Date());
-  console.log(secondsDifference, miniutesDiffernce, hoursDifference);
-  return isTomorrow(new Date(dateStr))
-    ? { text: "غداً", variant: "blue" }
-    : Math.abs(secondsDifference) < 60
+function createDateTime(dateStr: string, timeStr: string): Date {
+  console.log(dateStr, timeStr);
+  const date = new Date(dateStr);
+  const [hours, minutes, seconds] = timeStr.split(":").map(Number);
+
+  date.setHours(hours, minutes, seconds, 0);
+
+  return date;
+}
+
+const generateTimeDifferenceText = (
+  laterDate: Date,
+  earlierDate: Date
+): TBadgeTimeUpcomingLessons => {
+  const secondsDifference = differenceInSeconds(laterDate, earlierDate);
+  const miniutesDiffernce = differenceInMinutes(laterDate, earlierDate);
+  const hoursDifference = differenceInHours(laterDate, earlierDate);
+  return Math.abs(secondsDifference) < 60
     ? { text: `ثانية ${secondsDifference}`, variant: "blue" }
     : Math.abs(miniutesDiffernce) < 60
     ? { text: `دقيقة ${miniutesDiffernce}`, variant: "orange" }
     : Math.abs(hoursDifference) < 23
     ? { text: `دقيقة ${miniutesDiffernce}`, variant: "red" }
-    : { text: format(dateStr, "d MMMM", { locale: arEG }), variant: "red" };
+    : { text: format(earlierDate, "d MMMM", { locale: arEG }), variant: "red" };
+};
+
+export function getUpcomingLessonsBadgeText(
+  dateStr: string,
+  startTimeStr: string,
+  to: string
+): TBadgeTimeUpcomingLessons {
+  const startDateTime = createDateTime(dateStr, startTimeStr);
+  const endDateTime = createDateTime(dateStr, to);
+  if (differenceInSeconds(endDateTime, new Date()) < 0) {
+    return { text: "انتهت", variant: "blue" };
+  } else if (
+    isWithinInterval(new Date(), { start: startDateTime, end: endDateTime })
+  ) {
+    return { text: "الدرس جاري", variant: "orange" };
+  }
+  return isTomorrow(startDateTime)
+    ? { text: "غداً", variant: "blue" }
+    : generateTimeDifferenceText(startDateTime, new Date());
 }
