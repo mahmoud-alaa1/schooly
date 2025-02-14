a2e0b4c72bdd17679dd0e5572110d4311431e7d1
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-02-14 21:46:18 +0200

    feat: add UpcomingLessons and LessonSkeleton components; refactor UpcomingLessons to use new structure and implement pagination

---
+++ src/components/UpcomingLessons/UpcomingLessonsContent.tsx
@@ -0,0 +1,92 @@
+import Lesson from "./Lesson";
+import { TLesson } from "@/types/lessons";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+const fakeGetLessons = () => {
+  return new Promise<TLesson[]>((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: "1",
+          teacherId: "1",
+          subject: "الفيزياء",
+          grade: "الصف الثاني الثانوي",
+          title: "الحركة الدائرية",
+          lessonType: 0,
+          date: "2025-09-01",
+          from: "10:00",
+          to: "11:00",
+        },
+        {
+          id: "2",
+          teacherId: "1",
+          subject: "الفيزياء",
+          grade: "الصف الثاني الثانوي",
+          title: "الحركة الدائرية",
+          lessonType: 0,
+          date: "2025-09-01",
+          from: "10:00",
+          to: "11:00",
+        },
+        {
+          id: "3",
+          teacherId: "1",
+          subject: "الفيزياء",
+          grade: "الصف الثاني الثانوي",
+          title: "الحركة الدائرية",
+          lessonType: 0,
+          date: "2025-09-01",
+          from: "10:00",
+          to: "11:00",
+        },
+      ]);
+    }, 3000);
+  });
+};
+
+export default async function UpcomingLessonsContent() {
+  try {
+    const lessons: TLesson[] = await fakeGetLessons();
+    return (
+      <>
+        <ul className="p-6 pb-0">
+          {lessons.map((lesson) => (
+            <Lesson key={lesson.id} details={lesson} />
+          ))}
+        </ul>
+        <Pagination className="mb-2">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </>
+    );
+  } catch (error) {
+    console.error(error);
+    return <p className="p-4 text-red-600">حدث خطأ اثناء جلب الحصص القادمة</p>;
+  }
+}
