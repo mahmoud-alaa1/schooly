a1dbd65fd34185c54256eb5d3ce289b7c8db93b3
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-02-14 21:05:33 +0200

    feat: enhance UpcomingLessons component with error handling and no lessons message; add fixed positioning to SignoutButton

--- src/components/SignoutButton.tsx
+++ src/components/SignoutButton.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 export default function SignoutButton() {
   return (
     <Button
+    className="fixed bottom-1 right-1"
       onClick={() => {
         signOut();
       }}
--- src/components/UpcomingLessons.tsx
+++ src/components/UpcomingLessons.tsx
@@ -1,26 +1,40 @@
 import { getUpcomingLessons } from "@/services/lessons";
-
 import { AiOutlineClockCircle } from "react-icons/ai";
-
 import Lesson from "./Lesson";
 
-
 export default async function UpcomingLessons() {
-  const upComingLessons = await getUpcomingLessons();
+  try {
+    const { data: lessons } = await getUpcomingLessons();
 
-  const { data: lessons } = upComingLessons;
-
-  return (
-    <div className="max-w-lg mx-auto bg-white rounded-2xl border-2 border-neutral-200 ">
-      <h2 className="border-b border-neutral-200 flex items-center gap-x-1 py-4 px-6 font-medium">
-        <AiOutlineClockCircle className="text-xl " />
-        حصصك القادمة
-      </h2>
-      <ul className="p-6">
-        {lessons.map((val) => {
-          return <Lesson key={val.id} details={val} />;
-        })}
-      </ul>
-    </div>
-  );
+    return (
+      <div className="max-w-lg mx-auto bg-white rounded-2xl border-2 border-neutral-200">
+        <h2 className="border-b border-neutral-200 flex items-center gap-x-1 py-4 px-6 font-medium">
+          <AiOutlineClockCircle className="text-xl" />
+          حصصك القادمة
+        </h2>
+        <ul className="p-6">
+          {lessons.length > 0 ? (
+            lessons.map((val) => <Lesson key={val.id} details={val} />)
+          ) : (
+            <li className="text-center text-gray-500 py-4">
+              لا توجد حصص قادمة
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return (
+      <div className="max-w-lg mx-auto bg-white rounded-2xl border-2 border-neutral-200">
+        <h2 className="border-b border-neutral-200 flex items-center gap-x-1 py-4 px-6 font-medium">
+          <AiOutlineClockCircle className="text-xl" />
+          حصصك القادمة
+        </h2>
+        <div className="p-4 text-red-600">
+          حدث خطأ أثناء جلب البيانات، يرجى المحاولة مرة أخرى
+        </div>
+      </div>
+    );
+  }
 }
