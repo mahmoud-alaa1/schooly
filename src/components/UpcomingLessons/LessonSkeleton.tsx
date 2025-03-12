0252f8604b0e07b00a65eaff4591484594a7e7b4
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-03-13 00:08:19 +0200

    feat: update LessonSkeleton and UpcomingLessons components for improved loading states and pagination handling

--- src/components/UpcomingLessons/LessonSkeleton.tsx
+++ src/components/UpcomingLessons/LessonSkeleton.tsx
@@ -5,7 +5,7 @@ export default function LessonSkeleton() {
     <div className="mb-3 min-h-20 grid grid-cols-[auto_3fr_auto] gap-x-3 border-b pt-2 pb-4 last-of-type:border-none">
       <Skeleton className="h-[52px] w-[52px] " />
       <article>
-        <header className="mb-5">
+        <header className="mb-2">
           <h3 className="text-[#00000073]">
             <Skeleton className="h-[12px] w-[200px] " />
           </h3>
@@ -14,7 +14,7 @@ export default function LessonSkeleton() {
           <Skeleton className="h-[8px] w-[100px] " />
         </div>
       </article>
-      <Skeleton className="h-[30px] w-[50px] " />
+      <Skeleton className="h-[20px] w-[50px] " />
     </div>
   );
 }
--- src/components/UpcomingLessons/UpcomingLessons.tsx
+++ src/components/UpcomingLessons/UpcomingLessons.tsx
@@ -16,13 +16,16 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function UpcomingLessons() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState<number | null>(null);
+
   const { data, isLoading, error } = useUpcomingLessons(currentPage);
   const response = data ?? ({} as TUpcomingLessons);
 
   const lessons: TLesson[] = response.data || [];
-  const totalPage = !isLoading ? response.meta.totalPages : 0;
-  console.log(lessons);
-
+  if (!isLoading && !error && response.meta.totalPages && totalPages === null) {
+    setTotalPages(response.meta.totalPages);
+  }
+  console.log(totalPages);
   return (
     <div className="max-w-lg h-fit bg-white rounded-2xl border-2 border-neutral-200">
       <h2 className="border-b border-neutral-200 flex items-center gap-x-1 py-4 px-6 font-medium">
@@ -30,7 +33,7 @@ export default function UpcomingLessons() {
         حصصك القادمة
       </h2>
       {isLoading ? (
-        <div className="p-6">
+        <div className="px-6">
           <LessonSkeleton />
           <LessonSkeleton />
           <LessonSkeleton />
@@ -44,64 +47,62 @@ export default function UpcomingLessons() {
               <Lesson key={lesson.id} details={lesson} />
             ))}
           </ul>
-          <Pagination className="mb-2 px-2">
-            <PaginationContent>
-              <PaginationItem>
-                <Button
-                  aria-label="Go to previous page"
-                  size="default"
-                  className={cn("gap-1 pr-2.5")}
-                  variant="ghost"
-                  disabled={currentPage === data?.meta.totalPages}
-                  onClick={() => setCurrentPage((prev) => prev + 1)}
-                >
-                  <ChevronRight />
-                  التالي
-                </Button>
-              </PaginationItem>
-
-              <ul className="flex flex-row-reverse gap-1">
-                {Array.from({ length: 3 }).map((_, index) => {
-                  const pageNumber: number =
-                    (Math.ceil(currentPage / 3) - 1) * 3 + index + 1;
-                  return (
-                    pageNumber <= totalPage && (
-                      <PaginationItem key={`pagination` + index}>
-                        <Button
-                          onClick={() => setCurrentPage(pageNumber)}
-                          size="default"
-                          disabled={currentPage === pageNumber}
-                          className={cn(
-                            "disabled:opacity-100 disabled:cursor-not-allowed"
-                          )}
-                          variant={
-                            currentPage === pageNumber ? "outline" : "ghost"
-                          }
-                        >
-                          {pageNumber}
-                        </Button>
-                      </PaginationItem>
-                    )
-                  );
-                })}
-              </ul>
-              <PaginationItem>
-                <Button
-                  aria-label="Go to previous page"
-                  size="default"
-                  className={cn("gap-1 pr-2.5")}
-                  variant="ghost"
-                  onClick={() => setCurrentPage((prev) => prev - 1)}
-                  disabled={currentPage === 1}
-                >
-                  السابق
-                  <ChevronLeft />
-                </Button>
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
         </>
       )}
+      <Pagination className="mb-2 px-2">
+        <PaginationContent>
+          <PaginationItem>
+            <Button
+              aria-label="Go to previous page"
+              size="default"
+              className={cn("gap-1 pr-2.5")}
+              variant="ghost"
+              disabled={currentPage === data?.meta.totalPages}
+              onClick={() => setCurrentPage((prev) => prev + 1)}
+            >
+              <ChevronRight />
+              التالي
+            </Button>
+          </PaginationItem>
+
+          <ul className="flex flex-row-reverse gap-1">
+            {Array.from({ length: 3 }).map((_, index) => {
+              const pageNumber: number =
+                (Math.ceil(currentPage / 3) - 1) * 3 + index + 1;
+              return (
+                pageNumber <= totalPages! && (
+                  <PaginationItem key={`pagination` + index}>
+                    <Button
+                      onClick={() => setCurrentPage(pageNumber)}
+                      size="default"
+                      disabled={currentPage === pageNumber}
+                      className={cn(
+                        "disabled:opacity-100 disabled:cursor-not-allowed"
+                      )}
+                      variant={currentPage === pageNumber ? "outline" : "ghost"}
+                    >
+                      {pageNumber}
+                    </Button>
+                  </PaginationItem>
+                )
+              );
+            })}
+          </ul>
+          <PaginationItem>
+            <Button
+              aria-label="Go to previous page"
+              size="default"
+              className={cn("gap-1 pr-2.5")}
+              variant="ghost"
+              onClick={() => setCurrentPage((prev) => prev - 1)}
+              disabled={currentPage === 1}
+            >
+              السابق
+              <ChevronLeft />
+            </Button>
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   );
 }
