233e2de2a9bf5c17418e59daab080bf4f3a51f8c
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-03-15 05:07:52 +0200

    feat: update PostComments component to use AutosizeTextarea for dynamic resizing; refactor comment handling and improve layout in the comments section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import UpcomingLessons from "@/components/UpcomingLessons/UpcomingLessons";
 
 export default async function Page() {
   return (
-    <div className="grid grid-cols-[minmax(200px,220px)_auto_minmax(330px,400px)] gap-4">
+    <div className="grid grid-cols-[minmax(200px,220px)_minmax(400px,auto)_minmax(330px,400px)] gap-4">
       <SideNavbar />
       <Posts />
       <div className="flex flex-col gap-4">
