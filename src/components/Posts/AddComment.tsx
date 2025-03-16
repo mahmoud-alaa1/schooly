286195ce6cd7057595f715092de8e9247e11467f
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-03-16 18:34:45 +0200

    feat: implement UserInput component for reusable comment input; refactor AddComment to utilize new component

--- src/components/Posts/AddComment.tsx
+++ src/components/Posts/AddComment.tsx
@@ -1,9 +1,7 @@
-import React, { FormEvent } from "react";
-import { AutosizeTextarea } from "../ui/AutoResizeTextArea";
-import UserAvatar from "../ui/userAvatar";
-import { AiOutlineSend } from "react-icons/ai";
-import { Button } from "../ui/button";
+import { FormEvent, useState } from "react";
+
 import { UseMutationResult } from "@tanstack/react-query";
+import UserInput from "../UserInput";
 
 interface IAddComment {
   mutatedFunc: UseMutationResult<
@@ -19,50 +17,24 @@ interface IAddComment {
 }
 
 export default function AddComment({ mutatedFunc, id }: IAddComment) {
-  const [comment, setComment] = React.useState<string>("");
+  const [comment, setComment] = useState<string>("");
   const { error, isPending, mutate: sendComment } = mutatedFunc;
-  const handleAddition = (e: FormEvent<HTMLFormElement>) => {
+  const handleAddition = (e: FormEvent) => {
     e.preventDefault();
     sendComment({ newComment: comment.trim(), postId: id });
     if (!error) setComment("");
   };
 
   return (
-    <>
-      <div className="bg-primary/10 border border-primary/25  rounded-xl p-3 grid grid-cols-[auto_1fr]   gap-3 ">
-        <UserAvatar avatar={5} size={24} className="flex-grow-0" />
-        <form
-          className="flex gap-2"
-          action=""
-          onSubmit={(e) => handleAddition(e)}
-        >
-          <div className="flex h-fit w-full">
-            <label htmlFor={`comment-id-${id}`}>
-              <strong className="text-xs hide">أضف تعليق</strong>
-            </label>
-            <AutosizeTextarea
-              minHeight={1}
-              disabled={isPending}
-              id={`comment-id-${id}`}
-              value={comment}
-              placeholder="أضف تعليق هنا..."
-              className="  bg-primary/0 flex-grow  border-none"
-              onChange={(e) => {
-                setComment(e.target.value);
-              }}
-            />
-          </div>
-          <Button
-            disabled={isPending || comment.trim() === ""}
-            size="nothing"
-            type="submit"
-            className=" border bg-primary-foreground rounded-full hover:bg-primary-foreground border-neutral-300 shadow-md py-1 px-[10px] h-fit"
-          >
-            <AiOutlineSend className="text-black text-base" />
-          </Button>
-        </form>
-      </div>
-      {error && <p className="text-red-500 ">{error.message}</p>}
-    </>
+    <UserInput
+      error={error}
+      id={`comment-id-${id}`}
+      isPending={isPending}
+      label="أضف تعليق هنا..."
+      onSubmit={handleAddition}
+      value={comment}
+      setValue={setComment}
+      
+    />
   );
 }
