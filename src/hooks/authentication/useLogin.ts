6d2a39657bfb13e20cfd471cb81d5c7f46acbe4b
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-05-27 15:49:15 +0300

    Refactor useLogin hook to replace axios with fetch API for login requests, adding error handling and logging on success.

--- src/hooks/authentication/useLogin.ts
+++ src/hooks/authentication/useLogin.ts
@@ -1,21 +1,21 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginSchema } from "@/schemas/loginSchema";
 import { useAuth } from "@/store/auth";
-import api from "@/lib/axios";
 import { useRouter } from "next/navigation";
 export default function useLogin() {
   const { login } = useAuth();
   const router = useRouter();
   const mutation = useMutation({
     mutationFn: async (data: loginSchema) => {
-      const res = await api.post<ILoginResponse>(
-        `http://localhost:3000/api/auth/login`,
-        data
-      );
-
-      return res.data;
+      const res = await fetch(`/api/auth/login`, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      if (!res.ok) throw new Error("فشل تسجيل الدخول");
+      return res.json() as Promise<ILoginResponse>;
     },
     onSuccess: (data) => {
+      console.log(data);
       router.push("/");
       login(data.data);
     },
