19dee1396afe7e5a506b586bb710090c7569a18e
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-05-27 19:37:54 +0300

    Enhance layout and AuthLogo components by adjusting styles; add border and background to header, modify padding, and update text size and font weight in AuthLogo.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col  ">
-      <header className="flex items-center justify-start">
+    <div className="flex flex-col">
+      <header className="flex items-center justify-start border-b-2 border-[#02D496] bg-[#B1F2DE] p-7">
         <Logo />
       </header>
-      <main className="flex mt-6  justify-center">{children}</main>
+      <main className="mt-10 flex justify-center">{children}</main>
     </div>
   );
 }
--- src/components/Logos/AuthLogo.tsx
+++ src/components/Logos/AuthLogo.tsx
@@ -3,8 +3,8 @@ import { GraduationCap } from "lucide-react";
 export default function AuthLogo() {
   return (
     <div className="flex items-center gap-2 text-[#017553]">
-      <h1 className="text-2xl font-bold">سكولي</h1>
-      <GraduationCap className="text-[#017553] size-10" />
+      <GraduationCap className="size-7 text-[#017553]" />
+      <h1 className="text-xl font-medium">سكولي</h1>
     </div>
   );
 }
