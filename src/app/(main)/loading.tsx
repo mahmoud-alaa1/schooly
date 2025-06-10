71348c91d0714cb8c5e5535d077870ba8f203b14
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-06-11 01:09:31 +0300

    style: update loading component with improved background color and animation effects for better visual appeal

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -2,13 +2,12 @@ import { GraduationCap } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="z-50 flex items-center justify-center bg-white">
+    <div className="z-50 flex h-screen items-center justify-center bg-neutral-100">
       <div className="relative">
-        <span className="absolute inset-0 animate-ping rounded-full border border-gray-400" />
-        <span className="absolute inset-0 animate-ping rounded-full border border-gray-400 delay-200" />
-        <span className="absolute inset-0 animate-ping rounded-full border border-gray-400 delay-400" />
+        <span className="border-primary absolute inset-0 animate-ping rounded-full border duration-1000" />
+
         <div className="rounded-full bg-[#02D496] p-6">
-          <GraduationCap className="text-primary bg-primary size-11" />
+          <GraduationCap className="bg-primary size-11 text-white" />
         </div>
       </div>
     </div>
