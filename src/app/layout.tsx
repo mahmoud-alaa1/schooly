664341dcf6f005005fb1acace2a69dcb2468253f
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-06-19 19:41:49 +0300

    feat: update layout metadata and replace hero image for enhanced branding
    - Updated the title and description in layout metadata to reflect the educational focus in Arabic.
    - Added Open Graph and Twitter card metadata for improved social sharing.
    - Replaced the hero image with a new version for better visual representation.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,28 @@ import "./globals.css";
 import RootLayout from "@/layouts/RootLayout";
 
 export const metadata: Metadata = {
-  title: "Schooly",
-  description:
-    "Schooly is a platform for students to manage their school life.",
-  icons: {
-    icon: "/favicon.ico",
+  title: "Schooly - المنصة التعليمية",
+  description: "منصة تعليمية ذكية لإدارة الحصص، الواجبات، بسهولة.",
+  openGraph: {
+    title: "Schooly - المنصة التعليمية",
+    description: "منصة تعليمية ذكية لإدارة الحصص، الواجبات",
+    url: "https://schooly.vercel.app/",
+    siteName: "Schooly",
+    images: [
+      {
+        url: "https://schooly.vercel.app/hero.png",
+        width: 1200,
+        height: 630,
+        alt: "Schooly Platform Preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Schooly - المنصة التعليمية",
+    description: "منصة تعليمية ذكية لإدارة الحصص، الواجبات",
+    images: ["https://schooly.vercel.app/hero.png"],
   },
 };
 
